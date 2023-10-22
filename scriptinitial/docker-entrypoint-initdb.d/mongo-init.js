4dc5b97c1388d68287ab385644c2efc1288e123f
Paulo Jean Alves Da Silva <paulojeanalvesdasilva@Paulos-MacBook-Pro.local>
2023-10-22 12:06:50 -0300

    refactor: Neste Commit foi adicionado uso de memoria no docker-compose e ajusta a leitura e criacao das tabelas para a base de dados de exemplo.

--- scriptinitial/docker-entrypoint-initdb.d/mongo-init.js
+++ scriptinitial/docker-entrypoint-initdb.d/mongo-init.js
@@ -1,86 +1,64 @@
 db.createUser(
-    {
-        user: "admin",
-        pwd: "pass",
-        roles: [
-            {
-                role: "readWrite",
-                db: "sample_db"
-            }
-        ]
-    }
+  {
+    user: "admin",
+    pwd: "pass",
+    roles: [
+      {
+        role: "readWrite",
+        db: "fiap"
+      }
+    ]
+  }
 );
 
-db = db.getSiblingDB('sample_db');
-
-db.createCollection('sample_collection');
-
+//CRIACAODASTABELAS
+db = db.getSiblingDB('fiap');
 db.createCollection('categories');
 db.createCollection('products');
 
+//Inserindo infos na tabelas na tabelas
 var categories = db.categories.insertMany([
-    {
-      "description": "Sobremesa"
-    },
-    {
-      "description": "Acompanhamento"
-    },
-    {
-      "description": "Lanche"
-    },
-    {
-      "description": "Bebida"
-    }
-  ])
-
-console.log("TESTE DIEGO",JSON.stringify(categories.insertedIds[0]))
+  {
+    "description": "Sobremesa"
+  },
+  {
+    "description": "Acompanhamento"
+  },
+  {
+    "description": "Lanche"
+  },
+  {
+    "description": "Bebida"
+  }
+])
 
 var products = db.products.insertMany([
-    {
-      "name": "Milk-Shake",
-      "description": "Sorvete batido com leite",
-      "price": "18",
-      "image_url": "teste",
-      "category": categories.insertedIds[0]
-    },
-    {
-      "name": "Batata Frita",
-      "description": "batatas fritas em oleo",
-      "price": "20",
-      "image_url": "teste",
-      "category": categories.insertedIds[1]
-    },
-    {
-      "name": "Hamburguer",
-      "description": "pao com carne",
-      "price": "35",
-      "image_url": "teste",
-      "category": categories.insertedIds[2]
-    },
-    {
-      "name": "Coca cola",
-      "description": "bebida",
-      "price": "10",
-      "image_url": "teste",
-      "category": categories.insertedIds[3]
-    }
-  ])
-
-
-db.sample_collection.insertMany([
-    {
-        org: 'helpdev',
-        filter: 'EVENT_A',
-        addrs: 'http://rest_client_1:8080/wh'
-    },
-    {
-        org: 'helpdev',
-        filter: 'EVENT_B',
-        addrs: 'http://rest_client_2:8081/wh'
-    },
-    {
-        org: 'github',
-        filter: 'EVENT_C',
-        addrs: 'http://rest_client_3:8082/wh'
-    }
-]);
+  {
+    "name": "Milk-Shake",
+    "description": "Sorvete batido com leite",
+    "price": "18",
+    "image_url": "teste",
+    "category": categories.insertedIds[0]
+  },
+  {
+    "name": "Batata Frita",
+    "description": "batatas fritas em oleo",
+    "price": "20",
+    "image_url": "teste",
+    "category": categories.insertedIds[1]
+  },
+  {
+    "name": "Hamburguer",
+    "description": "pao com carne",
+    "price": "35",
+    "image_url": "teste",
+    "category": categories.insertedIds[2]
+  },
+  {
+    "name": "Coca cola",
+    "description": "bebida",
+    "price": "10",
+    "image_url": "teste",
+    "category": categories.insertedIds[3]
+  }
+])

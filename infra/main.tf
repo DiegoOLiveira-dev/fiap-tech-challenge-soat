ddbdf70b464779aa961e5e07f9feebb703fa798a
DiegoOLiveira-dev <63567959+DiegoOLiveira-dev@users.noreply.github.com>
2024-03-13 13:38:31 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -10,12 +10,11 @@ module "configmaps" {
 }
 
 # Import Databases
-module "databases" {
-  source = "./db"
-}
+# module "databases" {
+#  source = "./db"
+# }
 
 # Import Apps
 module "apps" {
   source = "./app"
-  depends_on = [module.databases]
 }

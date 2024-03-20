78fcb2544a6051d796f9a0aaef71f8c49794a42a
diego <diego@MacBook-Pro-de-diego.local>
2024-03-20 15:17:35 -0300

    teste unitario controller payment

--- src/@Core/payment/adapter/controllers/payment.controller.spec.ts
+++ src/@Core/payment/adapter/controllers/payment.controller.spec.ts
@@ -2,7 +2,6 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { SavePaymentController } from './payment.controller';
 import { SavePaymentUseCase } from '../../core/usecases/save-payment.use-case';
 import { GetPaymentUseCase } from '../../core/usecases/get-payment.use-case';
-import { PaymentModule } from '../../payments.module';
 import { GetPaymentByIdUseCase } from '../../core/usecases/get-payment-byID.use-case';
 import { UpdatePaymentUseCase } from '../../core/usecases/update-payment.use-case';
 
@@ -14,14 +13,6 @@ describe('AppController (e2e)', () => {
   let getPaymentByIdUseCase: GetPaymentByIdUseCase;
   let updatePaymentUseCase: UpdatePaymentUseCase;
 
-  // Crie um objeto com os valores desejados para o comando simulado
-  const commandMock = {
-    id_client: 'idClient',
-    id_order: 'idOrder',
-    status_payment: 'statusPayment',
-    toCommand: jest.fn(), // Mock do método toCommand
-  };
-
 
   beforeEach(async () => {
     savePaymentUseCaseMock = {
@@ -29,19 +20,31 @@ describe('AppController (e2e)', () => {
     } as unknown as SavePaymentUseCase;
 
     getPaymentUseCaseMock = {
-      getAllPayments: jest.fn(),
+      getAllPayments: jest.fn().mockResolvedValue({
+        "id_client": "123",
+        "id_order": "65b06f0a528feae34a2bdf50",
+        "status_payment": "false"
+      }),
     } as unknown as GetPaymentUseCase;
 
     getPaymentByIdUseCase = {
-      getOrderById: jest.fn(),
+      getOrderById: jest.fn().mockResolvedValue({
+        "id_client": "123",
+        "id_order": "65b06f0a528feae34a2bdf50",
+        "status_payment": "false"
+      }),
     } as unknown as GetPaymentByIdUseCase;
 
     updatePaymentUseCase = {
-      updateOrderById: jest.fn(),
+      updateOrderById: jest.fn().mockResolvedValue({
+        "id_client": "123",
+        "id_order": "65b06f0a528feae34a2bdf50",
+        "status_payment": "false"
+      }),
     } as unknown as UpdatePaymentUseCase;
 
     const module: TestingModule = await Test.createTestingModule({
-      imports: [PaymentModule],
+      imports: [],
       controllers: [SavePaymentController],
       providers: [
         {
@@ -69,4 +72,81 @@ describe('AppController (e2e)', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('sould call save method', () => {
+
+    const requestMock = {
+      id_client: '123',
+      id_order: '123',
+      status_payment:'true',
+      toCommand: jest.fn().mockReturnValue({
+        id_client: "123",
+        id_order: "65b06f0a528feae34a2bdf50",
+        status_payment: "false"
+      }),
+    };
+
+    // Act
+    controller.save(requestMock);
+
+    // Assert
+    expect(requestMock.toCommand).toHaveBeenCalled();
+    expect(savePaymentUseCaseMock.savePayment).toHaveBeenCalled();
+  });
+
+  it('sould call getAllPayments method', async () => {
+
+     // Arrange
+     const expectedResult = {
+      "id_client": "123",
+      "id_order": "65b06f0a528feae34a2bdf50",
+      "status_payment": "false"
+    }
+    
+     // Act
+     const result = await controller.getAllPayments();
+
+     // Assert
+     expect(getPaymentUseCaseMock.getAllPayments).toHaveBeenCalled();
+     expect(result).toEqual(expectedResult);
+  });
+
+  it('sould call getOrderById method', async () => {
+
+    // Arrange
+    const queryId = '123'
+    const expectedResult = {
+      "id_client": "123",
+      "id_order": "65b06f0a528feae34a2bdf50",
+      "status_payment": "false"
+    }
+   
+    // Act
+    const result = await controller.getOrderById(queryId);
+
+    // Assert
+    expect(getPaymentByIdUseCase.getOrderById).toHaveBeenCalled();
+    expect(result).toEqual(expectedResult);
+ });
+
+ it('sould call updateOrderById method', async () => {
+
+  // Arrange
+  const queryIdMock = '123'
+  const statusMock = 'false'
+  const expectedResult = {
+    "id_client": "123",
+    "id_order": "65b06f0a528feae34a2bdf50",
+    "status_payment": "false"
+  }
+ 
+  // Act
+  const result = await controller.updateOrderById(queryIdMock, statusMock);
+
+  // Assert
+  expect(updatePaymentUseCase.updateOrderById).toHaveBeenCalled();
+  expect(result).toEqual(expectedResult);
+});
+
+  
 });
